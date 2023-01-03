ddaa28b4c5d71ab7cf644a41344271e8f6856fa2
Seb887 <seb887.dev@gmail.com>
2023-01-03 21:18:52 +0100

    update playround

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const itemsArr = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
   const randomNumComputer = Math.floor(Math.random() * 3);
+  // console.log('computerSelection: ', itemsArr[randomNumComputer]);
   return itemsArr[randomNumComputer];
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === 'rock' && computerSelection === 'rock') {
-    console.log('Nobody wins!');
-  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    console.log('You lose! Paper beats Rock.');
+  if (playerSelection === 'rock' && computerSelection === 'paper') {
+    return 'You lose! Paper beats Rock.';
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    console.log('You lose! Paper beats Rock.');
+    return 'You win! Rock beats Scissors.';
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    return 'You win! Paper beats Rock.';
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    return 'You lose! Rock beats Scissors.';
+  } else {
+    return 'Nobody wins!';
   }
 }
 
